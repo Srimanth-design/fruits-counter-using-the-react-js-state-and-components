680518096f64b64be1c4bf082546c2d452f02601
Srimanth <srimanthraviprasad@gmail.com>
2021-06-20 07:37:15 +0000

    Solution of the fruits counter using the react js state and components

---
+++ src/components/FruitsCounter/index.js
@@ -0,0 +1,65 @@
+import {Component} from 'react'
+import './index.css'
+
+class FruitsCounter extends Component {
+  state = {mangoCount: 0, bananaCount: 0}
+
+  onClickMango = () => {
+    this.setState(prevState => ({mangoCount: prevState.mangoCount + 1}))
+  }
+
+  onClickBanana = () => {
+    this.setState(prevState => ({bananaCount: prevState.bananaCount + 1}))
+  }
+
+  render() {
+    const {mangoCount, bananaCount} = this.state
+
+    return (
+      <div className="fruits-counter-container">
+        <div className="fruits-counter">
+          <h1 className="count-text">
+            Bob ate <span className="count">{mangoCount}</span> mangoes
+            <span className="count"> {bananaCount}</span> bananas
+          </h1>
+          <div className="counters-control-container">
+            <div className="counter-control">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/mango-img.png"
+                alt="mango"
+                className="fruit-image"
+              />
+              <div className="button-container">
+                <button
+                  type="button"
+                  className="button"
+                  onClick={this.onClickMango}
+                >
+                  Eat Mango
+                </button>
+              </div>
+            </div>
+            <div className="counter-control">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/banana-img.png"
+                alt="banana"
+                className="fruit-image"
+              />
+              <div className="button-container">
+                <button
+                  type="button"
+                  className="button"
+                  onClick={this.onClickBanana}
+                >
+                  Eat Banana
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FruitsCounter
